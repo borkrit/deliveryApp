1a6555e776d200739d8c1051a3599b0544b2c961
Vladyslav Pohorilyi <vladyslav.pohorilyi@gmail.com>
2023-05-30 18:42:52 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ const app = express();
 const db = mysql.createConnection({
     host: process.env.DB_HOST ,
     user: process.env.DB_USER ,
-    password:process.env.DB_HOST ,
-    database: process.env.DB_PASSWORD ,
+    password:process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME ,
     port: process.env.DB_PORT 
     
 }) 
